248efdbb9d901dcd8dc19709e38655f3a86e4cc1
Islam <islamxdaamduu@gmail.com>
2023-05-20 14:02:01 +0600

    added cards

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -26,7 +26,7 @@ function HomePage() {
             <span></span>
           </div>
           <p>
-            Комплексное развитие сферы электротранспорта и электрозарядной{" "}
+            Комплексное развитие сферы электротранспорта и электрозарядной
             <br /> инфраструктуры для создания комфортных условий для всех
             участников сферы.
           </p>
@@ -83,32 +83,93 @@ function HomePage() {
         </div>
       </div>
 
-      <div className="home_section-project">
-        <div className="home_section-project-title">
-          <h2>Наши проекты</h2>
-          <p>В данный момент мы реализуем следующие проекты</p>
+      <div className="home_section-how-we-works">
+        <div className="title__how-we-works">
+          Как мы работаем
         </div>
-        <div className="cards">
-          <div className="item_card-1">
-            <img
-              src="https://thumb.tildacdn.com/tild3430-6432-4233-a632-613033626631/-/cover/720x504/center/center/-/format/webp/image.png"
-              alt=""
-            />
-            <h2>
-              Контракт с распред <br /> компанией"НурАл Энерго"
-            </h2>
-            <p>
-              В рамках выставки EXPO2023 организованный <br /> ТПП КР, было
-              подписано соглашение об установке <br /> зарядных станций га
-              территории ТЦ "АЮ Гранд"
-            </p>
+        <div className="how-we-works__items">
+          <div className="stepsBlock">
+            <div className="stepsBlock__items">
+              <div className="stepsAboveBlock">
+                <div className="steps__number">
+                  1
+                </div>
+                <div className="steps__line"></div>
+              </div>
+              <div className="stepsContents">
+                <span className="stepsContentsTitle">Выбор устройств и согласование условий</span>
+                <p className="stepsContentsText">Менеджер проконсультирует вас, составит коммерческое предложение и презентует наши решения.</p>
+              </div>
+            </div>
+            <div className="stepsBlock__items">
+              <div className="stepsAboveBlock">
+                <div className="steps__number">
+                  2
+                </div>
+                <div className="steps__line"></div>
+              </div>
+              <div className="stepsContents">
+                <span className="stepsContentsTitle">Монтаж и подписание договора</span>
+                <p className="stepsContentsText">В удобное вам время мастер установит станцию, проинструктирует, как ее использовать, и вы подпишете договор.</p>
+              </div>
+            </div>
+            <div className="stepsBlock__items">
+              <div className="stepsAboveBlock">
+                <div className="steps__number">
+                  3
+                </div>
+                <div className="steps__line"></div>
+              </div>
+              <div className="stepsContents">
+                <span className="stepsContentsTitle">Круглосуточная техподдержка</span>
+                <p className="stepsContentsText">24 часа в сутки наша техподдержка будет отслеживать работу вашей сети ЭЗС и контролировать безопасность.</p>
+              </div>
+            </div>
+            <div className="stepsBlock__items">
+              <div className="stepsAboveBlock">
+                <div className="steps__number">
+                  4
+                </div>
+              </div>
+              <div className="stepsContents">
+                <span className="stepsContentsTitle">Выезд на локацию</span>
+                <p className="stepsContentsText">В случае поломки наши техспециалисты выедут на локацию, осмотрит объект на предмет технической неполадки.</p>
+              </div>
+            </div>
           </div>
-          {/* <div className="item_card-2">
-            <h2>
-              Установка станции на <br /> парковке БЦ 79
-            </h2>
-            <p>Установлено 2 станции по 120 квт. </p>
-          </div> */}
+        </div>
+      </div>
+
+      <div className="home_section-project">
+        <div className="containerC">
+          <div className="home_section-project-title">
+            <h2>Наши проекты</h2>
+            <p>В данный момент мы реализуем следующие проекты</p>
+          </div>
+          <div className="cards">
+            <div className="cards__item">
+              <img src="https://thumb.tildacdn.com/tild3430-6432-4233-a632-613033626631/-/cover/720x504/center/center/-/format/webp/image.png" className="cards__img"/>
+              <div className="cards__typography">
+                <div className="cards__title">Контракт с распред компанией"НурАл Энерго"</div>
+                <div className="cards__text">В рамках выставки EXPO2023 организованный ТПП КР, было подписано соглашение об установке зарядных станций га территории ТЦ "АЮ Гранд"</div>
+              </div>
+            </div>
+            <div className="cards__item__special">
+              <img src="https://thumb.tildacdn.com/tild6531-6339-4363-b130-316538633965/-/cover/720x990/center/center/-/format/webp/IMG_4148.jpeg" className="cards__img__special"/>
+              <div className="blackBlock"></div>
+              <div className="cards__typography__special">
+                <div className="cards__title__special">Установка станции на парковке БЦ 79</div>
+                <div className="cards__text__special">Установлено 2 станции по 120 квт.</div>
+              </div>
+            </div>
+            <div className="cards__item">
+              <img src="https://thumb.tildacdn.com/tild3338-3238-4131-a638-373761666639/-/cover/720x504/center/center/-/format/webp/image.png" className="cards__img"/>
+              <div className="cards__typography">
+                <div className="cards__title">Произвели монтаж зарядной станции на площадке ресторана "President Hall"</div>
+                <div className="cards__text">Инструкция для экстренной ситуации</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
