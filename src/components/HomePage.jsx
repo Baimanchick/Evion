ae39b5ca60e2b83149d553f5aae1d4d038bb6799
Your Name <sovetbekovaajar@gmail.com>
2023-05-24 12:53:26 +0600

    media

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -548,10 +548,23 @@ function HomePage() {
                 <span>политика конфиденциальности</span>
               </div>
               <div className="footer__info__social">
-                <ion-icon name="logo-facebook"></ion-icon>
-                <ion-icon name="logo-instagram"></ion-icon>
-                <ion-icon name="logo-youtube"></ion-icon>
-                <ion-icon name="logo-whatsapp"></ion-icon>
+                <a href="https://www.facebook.com/people/Evion-KG/pfbid0yLWBFJELw9xAZhuXhvftWacprnuNeJ7cbYwS6HHJKUpH6G4jcimTkR7FB9D2cQLwl/?mibextid=ZbWKwL">
+                  {" "}
+                  <ion-icon name="logo-facebook"></ion-icon>
+                </a>
+
+                <a href="https://instagram.com/evion_kg?igshid=MzRlODBiNWFlZA==">
+                  {" "}
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a href="https://youtube.com/@EVIONKG">
+                  {" "}
+                  <ion-icon name="logo-youtube"></ion-icon>
+                </a>
+                <a href="https://api.whatsapp.com/send/?phone=996500333351&text&type=phone_number&app_absent=0">
+                  {" "}
+                  <ion-icon name="logo-whatsapp"></ion-icon>
+                </a>
               </div>
             </div>
           </div>
