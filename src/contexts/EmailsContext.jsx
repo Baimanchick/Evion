3cd28e2b5ade5855331d31214354af7377ed593c
Baimuratik <baikaabdalim@gmail.com>
2023-05-25 15:08:04 +0600

    added auth

--- src/contexts/EmailsContext.jsx
+++ src/contexts/EmailsContext.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import React, { createContext, useContext, useReducer, useState } from 'react';
-import { ACTIONS, API, BASE_URL, LIMIT, SPAMS_URL } from '../utils/consts';
+import axios from "axios";
+import React, { createContext, useContext, useReducer, useState } from "react";
+import { ACTIONS, API, BASE_URL, LIMIT, SPAMS_URL } from "../utils/consts";
 
 const emailContext = createContext();
 
@@ -9,25 +9,25 @@ export function useEmailContext() {
 }
 
 const initState = {
-    emails: [],
-    oneEmail: null,
-    spamsDB: []
-}
+  emails: [],
+  oneEmail: null,
+  spamsDB: [],
+};
 
 const spams = [];
 
 function reducer(state, action) {
-    switch (action.type) {
-        case ACTIONS.emails:
-            return {...state, emails: action.payload}
-        case ACTIONS.oneEmail:
-            return {...state, oneEmail: action.payload}
-        case ACTIONS.spams:
-            return {...state, spams: action.payload}
-  
-        default:
-            return state
-    }
+  switch (action.type) {
+    case ACTIONS.emails:
+      return { ...state, emails: action.payload };
+    case ACTIONS.oneEmail:
+      return { ...state, oneEmail: action.payload };
+    case ACTIONS.spams:
+      return { ...state, spams: action.payload };
+
+    default:
+      return state;
+  }
 }
 
 function EmailContext({ children }) {
@@ -35,11 +35,11 @@ function EmailContext({ children }) {
   // const [currentPage, setCurrentPage] = useState(1);
   async function getEmails() {
     try {
-        const res = await axios.get(`${BASE_URL}`);
-        dispatch({
-            type: ACTIONS.emails,
-            payload: res.data
-        })
+      const res = await axios.get(`${BASE_URL}`);
+      dispatch({
+        type: ACTIONS.emails,
+        payload: res.data,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -47,12 +47,12 @@ function EmailContext({ children }) {
 
   async function getSpams() {
     try {
-        const res = await axios.get(`${SPAMS_URL}`);
-        console.log(res.data)
-        dispatch({
-            type: ACTIONS.spamsDB,
-            payload: res.data
-        })
+      const res = await axios.get(`${SPAMS_URL}`);
+      console.log(res.data);
+      dispatch({
+        type: ACTIONS.spamsDB,
+        payload: res.data,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -63,7 +63,7 @@ function EmailContext({ children }) {
       await axios.post(`${BASE_URL}`, newEmail);
       getEmails();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
@@ -73,65 +73,14 @@ function EmailContext({ children }) {
       spams.push(res.data);
       dispatch({
         type: ACTIONS.spamsDB,
-        payload: spams
-      })
-      // console.log(state.emails[id - 1]);
+        payload: spams,
+      });
       await axios.delete(`${BASE_URL}/${ID}`);
       getEmails();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-
-  // async function getOneEmail(id) {
-  //   try {
-  //       const { data } = await axios.get(`${API}/${id}`);
-  //       console.log(data)
-  //       dispatch({
-  //           type: ACTIONS.oneFilm,
-  //           payload: data
-  //       })
-  //   } catch (error) {
-  //       console.log(error)
-  //   }
-  // }
-
-  // async function addReview(newReview) {
-  //   try {
-  //       await axios.post(API, newReview);
-  //       getMovies();
-  //   } catch (error) {   
-  //       console.log(error)
-  //   }
-  // }
-
-  // async function deleteReview(id) {
-  //   try {
-  //       await axios.delete(`${API}/${id}`);
-  //       getMovies()
-  //   } catch (error) {
-  //       console.log(error)
-  //   }
-  // }
-
-  // async function editReview(id, newReview) {
-  //   try {
-  //       await axios.patch(`${API}/${id}`, newReview);
-  //       getMovies();
-  //   } catch (error) {
-  //       console.log(error)
-  //   }
-  // }
-
-  // async function addComment(id, newFilm) {
-  //   try {
-  //       await axios.patch(`${API}/${id}`, newFilm);
-  //       // data.comments.push(newComment)
-  //       getMovies();
-  //   } catch (error) {
-  //       console.log(error)
-  //   }
-  // }
 
   const value = {
     emails: state.emails,
@@ -140,10 +89,12 @@ function EmailContext({ children }) {
     addEmails,
     spamEmails,
     getSpams,
-    spams: state.spamsDB
-  }
+    spams: state.spamsDB,
+  };
 
-  return <emailContext.Provider value={value}>{children}</emailContext.Provider>;
+  return (
+    <emailContext.Provider value={value}>{children}</emailContext.Provider>
+  );
 }
 
-export default EmailContext;  
+export default EmailContext;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import EmailContext from "./contexts/EmailsContext";
+import AuthContext from "./contexts/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <EmailContext>
-      <App />
+      <AuthContext>
+        <App />
+      </AuthContext>
     </EmailContext>
   </BrowserRouter>
 );
