80dd6f8cc59e1d2ff7b5da4d11c9ac3c57b3c413
Baimuratik <baikaabdalim@gmail.com>
2023-06-01 15:25:27 +0600

    deployed our db

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "http://localhost:8000/emails";
-export const SPAMS_URL = "http://localhost:8000/spams";
-export const BLOGS_URL = "http://localhost:8000/blogs";
-export const PHONES_URL = "http://localhost:8000/phones";
-export const PHONES_URL2 = "http://localhost:8000/phones2";
+export const BASE_URL = "https://evion-db-main.vercel.app/emails";
+export const SPAMS_URL = "https://evion-db-main.vercel.app/spams";
+export const BLOGS_URL = "https://evion-db-main.vercel.app/blogs";
+export const PHONES_URL = "https://evion-db-main.vercel.app/phones";
+export const PHONES_URL2 = "https://evion-db-main.vercel.app/phones2";
 
 export const ACTIONS = {
   emails: "emails",
@@ -14,7 +14,7 @@ export const ACTIONS = {
   phones: "phones",
   onePhone: "onePhone",
   phones2: "phones2",
-  onePhone2: "onePhone2"
+  onePhone2: "onePhone2",
 };
 
 // export const ADMINS = ["baika132213@gmail.com"];
